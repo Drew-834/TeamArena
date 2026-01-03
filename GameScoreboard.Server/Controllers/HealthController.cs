72aa829b47653c0315e121fa2703bda2dc217f5f
Drew-834 <Amcmusic37@gmail.com>
2026-01-02 20:40:39 -0500

    Add diagnostic endpoint to debug database connection

--- GameScoreboard.Server/Controllers/HealthController.cs
+++ GameScoreboard.Server/Controllers/HealthController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GameScoreboard.Server.Data;
 
 namespace GameScoreboard.Server.Controllers;
 
@@ -6,6 +8,42 @@ namespace GameScoreboard.Server.Controllers;
 [Route("api/[controller]")]
 public class HealthController : ControllerBase
 {
+    private readonly AppDbContext _db;
+    private readonly IConfiguration _config;
+
+    public HealthController(AppDbContext db, IConfiguration config)
+    {
+        _db = db;
+        _config = config;
+    }
+
     [HttpGet]
     public IActionResult Get() => Ok(new { status = "ok" });
+
+    [HttpGet("db")]
+    public async Task<IActionResult> CheckDb()
+    {
+        try
+        {
+            // Try to connect to database
+            var canConnect = await _db.Database.CanConnectAsync();
+            var memberCount = canConnect ? await _db.TeamMembers.CountAsync() : -1;
+            
+            return Ok(new { 
+                canConnect, 
+                memberCount,
+                hasConnectionString = !string.IsNullOrEmpty(_config.GetConnectionString("DefaultConnection")),
+                provider = _db.Database.ProviderName
+            });
+        }
+        catch (Exception ex)
+        {
+            return Ok(new { 
+                canConnect = false, 
+                error = ex.Message,
+                innerError = ex.InnerException?.Message,
+                hasConnectionString = !string.IsNullOrEmpty(_config.GetConnectionString("DefaultConnection"))
+            });
+        }
+    }
 } 
